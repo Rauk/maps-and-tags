9419c8424f7c4de0a77bbbfc5b2ba6fe5b48102b
Rauk <17raunak@gmail.com>
2016-02-25 12:09:15 +0530

    corrections

--- js/myscripts.js
+++ js/myscripts.js
@@ -7,11 +7,11 @@ function initMap() {
 }
     
     
-var metroPolitanCities = {
-    {"Hyderabad"   : 17.3700, 78.4800}, 
-    {"New Delhi"   : 28.6139, 77.2090},
-    {"Kolkata"     : 22.5667, 88.3667},
-    {"Chennai"     : 13.0827, 80.2707},
-    {"Mumbai"      : 18.9750, 72.8258},
-    {"Bengaluru"   : 12.9667, 77.5667}
-}
+//var metroPolitanCities = {
+//    {"Hyderabad"   : 17.3700, 78.4800}, 
+//    {"New Delhi"   : 28.6139, 77.2090},
+//    {"Kolkata"     : 22.5667, 88.3667},
+//    {"Chennai"     : 13.0827, 80.2707},
+//    {"Mumbai"      : 18.9750, 72.8258},
+//    {"Bengaluru"   : 12.9667, 77.5667}
+//}
