e4470f0e4cba0fbc628604a09a7be10a0dbd42d2
Rauk <17raunak@gmail.com>
2016-02-25 12:55:02 +0530

    reduced comments

--- js/myscripts.js
+++ js/myscripts.js
@@ -4,20 +4,16 @@ function initMap() {
         center: {lat: 21.0000, lng: 78.0000},
         zoom: 5
     });
-    
-    // Create the search box and link it to the UI element.
+
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
       searchBox.setBounds(map.getBounds());
     });
 
     var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
     searchBox.addListener('places_changed', function() {
       var places = searchBox.getPlaces();
 
@@ -25,13 +21,11 @@ function initMap() {
         return;
       }
 
-      // Clear out the old markers.
       markers.forEach(function(marker) {
         marker.setMap(null);
       });
       markers = [];
 
-      // For each place, get the icon, name and location.
       var bounds = new google.maps.LatLngBounds();
       places.forEach(function(place) {
         var icon = {
@@ -42,7 +36,6 @@ function initMap() {
           scaledSize: new google.maps.Size(25, 25)
         };
 
-        // Create a marker for each place.
         markers.push(new google.maps.Marker({
           map: map,
           icon: icon,
@@ -51,7 +44,6 @@ function initMap() {
         }));
 
         if (place.geometry.viewport) {
-          // Only geocodes have viewport.
           bounds.union(place.geometry.viewport);
         } else {
           bounds.extend(place.geometry.location);
@@ -60,13 +52,3 @@ function initMap() {
       map.fitBounds(bounds);
     });
 }
-    
-    
-//var metroPolitanCities = {
-//    {"Hyderabad"   : 17.3700, 78.4800}, 
-//    {"New Delhi"   : 28.6139, 77.2090},
-//    {"Kolkata"     : 22.5667, 88.3667},
-//    {"Chennai"     : 13.0827, 80.2707},
-//    {"Mumbai"      : 18.9750, 72.8258},
-//    {"Bengaluru"   : 12.9667, 77.5667}
-//}
